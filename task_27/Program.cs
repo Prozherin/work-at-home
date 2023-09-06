05156407d2dd51816d1290ba2bafea9140fea078
Prozherin <taskacik@gmail.com>
2023-09-06 22:07:37 +0500

    add task 27 принимает на вход число и выдаёт сумму цифр в числе.

---
+++ task_27/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите целое число:");
+int number = Int32.Parse(Console.ReadLine());
+
+int sumOfDigits = 0;
+while (number > 0)
+{
+    sumOfDigits += number % 10;
+    number /= 10;
+}
+
+Console.WriteLine("Сумма чисел равна " + sumOfDigits.ToString() + ".");
